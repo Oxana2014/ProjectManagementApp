95b284727271bd1cd681d7b45f6c21f39c28c504
Oksana Kormilina <keysb@mail.ru>
2024-01-16 17:48:54 +0400

    manage Add New Project state

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,15 @@ goal: "Learn React from the group up",
 description: "Start with the basics, finish with advanced knowledge"}
 
 function App() {
+  const [addProject, setAddProject] = useState(false)
 const [projects, setProjects] = useState(PROJECTS)
+
+function addProjectButtonHandle() {
+  setAddProject(true)
+}
+function cancelAddingProject() {
+  setAddProject(false)
+}
 
 function addProjectHandle(newProject) {
   setProjects(prevProjects => {
@@ -24,10 +32,10 @@ return curProjects
     <>
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 flex gap-8">
-        <Sidebar projects={projects} />
-        {projects.length === 0 && <NoProjects />}
-<NewProject />
-       {/* {projects.length > 0 && <Project project={PROJ}/>} */}
+        <Sidebar projects={projects} onAddProject={addProjectButtonHandle}/>
+        {addProject && <NewProject onCancel={cancelAddingProject} />}
+        {!addProject && projects.length === 0 && <NoProjects />}
+       {!addProject && projects.length > 0 && <Project project={PROJ}/>}
       </main>
     </>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,9 +1,9 @@
-export default function NewProject() {
+export default function NewProject({onCancel}) {
   return (
     <form className="mt-4 ">
        <div className="w-[35rem] mt-16 ">
        <menu className="flex items-center justify-end gap-4 my-4">       
-          <button className="text-stone-800 hover:text-stone-950">
+          <button onClick={onCancel} className="text-stone-800 hover:text-stone-950">
             Cancel
           </button>
           <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
