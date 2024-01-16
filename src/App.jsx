d61d89ad469ba45e4a39f57738267d28e856124d
Oksana Kormilina <keysb@mail.ru>
2024-01-16 15:33:49 +0400

    Add state managing for projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import {useState} from 'react'
+
 import NoProjects from "./components/NoProjects";
 import Sidebar from "./components/Sidebar";
 
+const PROJECTS = ['Project 1', 'React']
+
 function App() {
+const [projects, setProjects] = useState(PROJECTS)
+
+function addProjectHandle(newProject) {
+  setProjects(prevProjects => {
+    const curProjects = [newProject, ...prevProjects]
+return curProjects
+  })
+}
   return (
     <>
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 flex gap-8">
         <Sidebar />
-       <NoProjects />
+        {projects.length === 0 && <NoProjects />}
+       {projects.length > 0 && <h2>{projects[0]}</h2>}
       </main>
     </>
   );
