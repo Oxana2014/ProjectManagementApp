e48af00aa354a3fc65132599f57b403a4b244eec
Oksana Kormilina <keysb@mail.ru>
2024-01-16 15:20:02 +0400

    Add Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import NoProjects from "./components/NoProjects";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
       {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
       <main className="h-screen my-8 flex gap-8">
+        <Sidebar />
        <NoProjects />
       </main>
     </>
