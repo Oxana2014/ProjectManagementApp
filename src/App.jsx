f83c408b61b1f2ccde7a55168856c7cd097a32d8
Oksana Kormilina <keysb@mail.ru>
2024-01-18 19:33:19 +0400

    Clear completely for class version

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,7 @@
-import { useState } from "react";
-
-import NoProjects from "./components/NoProjects";
-import Sidebar from "./components/Sidebar";
-import Project from "./components/Project";
-import NewProject from "./components/NewProject";
-
-const PROJ = {
-  title: "Learning React",
-  date: new Date().toLocaleDateString(),
-  description:
-    "Learn React from the group up <br></br>Start with the basics, finish with advanced knowledge",
-};
-
 function App() {
-  const [addProject, setAddProject] = useState(false);
-  const [projects, setProjects] = useState([PROJ]);
-  const [highlighted, setHighlighted] = useState(0);
-
-  function handleChangeHighlighted(index) {
-    setHighlighted(index);
-  }
-  function addProjectButtonHandle() {
-    setAddProject(true);
-  }
-  function cancelAddingProject() {
-    setAddProject(false);
-  }
-
-  function addProjectHandle(newProject) {
-    setProjects((prevProjects) => {
-      const curProjects = [newProject, ...prevProjects];
-      console.log(curProjects);
-      return curProjects;
-    });
-    setAddProject(false);
-  }
-
-  function deleteProjectHandle(index) {
-    setProjects((prevProjects) => {
-      const curProjects = [];
-      for (let i = 0; i < prevProjects.length; i++) {
-        if (i !== index) curProjects.push(prevProjects[i]);
-      }
-      return curProjects;
-    });
-    setHighlighted(0)
-  }
-
   return (
     <>
-      {/* <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> */}
-      <main className="h-screen my-8 flex gap-8">
-        <Sidebar
-          projects={projects}
-          onAddProject={addProjectButtonHandle}
-          onChangeHighlighted={handleChangeHighlighted}
-          highlighted={highlighted}
-        />
-        {addProject && (
-          <NewProject
-            onCancel={cancelAddingProject}
-            onSubmit={addProjectHandle}
-          />
-        )}
-        {!addProject && projects.length === 0 && (
-          <NoProjects onAddProject={addProjectButtonHandle} />
-        )}
-        {!addProject && projects.length > 0 && (
-          <Project project={projects[highlighted]} highlighted={highlighted} onDelete={deleteProjectHandle} />
-        )}
-      </main>
+      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>  
     </>
   );
 }
